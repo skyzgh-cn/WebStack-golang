30ce858c178f4b0082910988bf9cb151f380ca3a
skyzgh-cn <skyzgh@gmail.com>
2025-05-12 01:17:27 +0800

    Release v3

--- models/config.go
+++ models/config.go
@@ -2,9 +2,7 @@ package models
 
 import (
 	"encoding/json"
-	"io/ioutil"
 	"os"
-	"strconv"
 	"sync"
 )
 
@@ -14,10 +12,10 @@ type Config struct {
 		Name   string `json:"name"`
 		Host   string `json:"host"`
 		Port   string `json:"port"`
-		Dbtype string `json:"dbtype"`
+		Dbtype string `json:"dbtype"` // 新增字段
 	} `json:"app"`
 	Mysql struct {
-		Type      string `json:"type"`
+		Type      string `json:"type"` // 可删，实际由dbtype决定
 		Host      string `json:"host"`
 		Port      string `json:"port"`
 		User      string `json:"user"`
@@ -37,86 +35,24 @@ type Config struct {
 var (
 	configInstance *Config
 	loadOnce       sync.Once
-	jsonConfig     *Config // 存储从config.json读取的配置
 )
 
-// 从config.json文件加载配置
-func loadConfigFromFile(filePath string) (*Config, error) {
-	data, err := ioutil.ReadFile(filePath)
-	if err != nil {
-		return nil, err
-	}
-
-	var config Config
-	err = json.Unmarshal(data, &config)
-	if err != nil {
-		return nil, err
-	}
-
-	return &config, nil
-}
-
-// getEnv 获取环境变量，如果不存在则返回config.json中的值，如果config.json也不存在则返回默认值
-func getEnv(key string, jsonValue string, defaultValue string) string {
-	value := os.Getenv(key)
-	if value == "" {
-		if jsonValue != "" {
-			return jsonValue
-		}
-		return defaultValue
-	}
-	return value
-}
-
-// getEnvBool 获取布尔类型的环境变量
-func getEnvBool(key string, jsonValue bool, defaultValue bool) bool {
-	value := os.Getenv(key)
-	if value == "" {
-		return jsonValue
-	}
-	boolValue, err := strconv.ParseBool(value)
-	if err != nil {
-		return jsonValue
-	}
-	return boolValue
-}
-
-// LoadConfig 函数从环境变量和config.json加载配置
+// LoadConfig 函数用于从 config.json 文件加载配置
 func LoadConfig() (*Config, error) {
-	loadOnce.Do(func() {
-		// 尝试从config.json加载配置
-		fileConfig, err := loadConfigFromFile("config.json")
+	loadOnce.Do(func() { // 确保只加载一次配置
+		file, err := os.ReadFile("./config.json") // 读取配置文件
 		if err != nil {
-			// 如果加载失败，使用空配置
-			fileConfig = &Config{}
+			return
 		}
-		jsonConfig = fileConfig
 
-		config := &Config{}
+		var config Config
+		err = json.Unmarshal(file, &config) // 解析 JSON 数据到 Config 结构体
+		if err != nil {
+			return
+		}
 
-		// 加载App配置
-		config.App.Name = getEnv("APP_NAME", jsonConfig.App.Name, "WebStack")
-		config.App.Host = getEnv("APP_HOST", jsonConfig.App.Host, "0.0.0.0")
-		config.App.Port = getEnv("APP_PORT", jsonConfig.App.Port, "8080")
-		config.App.Dbtype = getEnv("DB_TYPE", jsonConfig.App.Dbtype, "sqlite")
-
-		// 加载MySQL配置
-		config.Mysql.Type = "mysql"
-		config.Mysql.Host = getEnv("MYSQL_HOST", jsonConfig.Mysql.Host, "127.0.0.1")
-		config.Mysql.Port = getEnv("MYSQL_PORT", jsonConfig.Mysql.Port, "3306")
-		config.Mysql.User = getEnv("MYSQL_USER", jsonConfig.Mysql.User, "webstack")
-		config.Mysql.Password = getEnv("MYSQL_PASSWORD", jsonConfig.Mysql.Password, "webstack")
-		config.Mysql.Database = getEnv("MYSQL_DATABASE", jsonConfig.Mysql.Database, "webstack")
-		config.Mysql.Charset = getEnv("MYSQL_CHARSET", jsonConfig.Mysql.Charset, "utf8mb4")
-		config.Mysql.ParseTime = getEnvBool("MYSQL_PARSE_TIME", jsonConfig.Mysql.ParseTime, true)
-		config.Mysql.Loc = getEnv("MYSQL_LOC", jsonConfig.Mysql.Loc, "Local")
-
-		// 加载SQLite配置
-		config.Sqlite.Type = "sqlite"
-		config.Sqlite.File = getEnv("SQLITE_FILE", jsonConfig.Sqlite.File, "./webstack.db")
-
-		configInstance = config
+		configInstance = &config // 缓存配置实例
 	})
 
-	return configInstance, nil
+	return configInstance, nil // 返回缓存的实例
 }
